f4d3800bbaf4014171ac40942cd0be8f87523c6a
Kronos <kronossolucionestic@gmail.com>
2019-02-15 09:47:23 -0500

    Inicial burgos

--- view/index.php
+++ view/index.php
@@ -5,6 +5,40 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+<<<<<<< HEAD
 	<h12>Hola Mundo</h2>
+=======
+  <header>
+	 <div class="container">
+		 
+		
+		 
+  <div class="row">
+
+	  
+	 <div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="media">
+  <img src="imas/iconwhatsapp.png" class="align-self-center mr-3" alt="...">
+  <div class="media-body">
+    <h5 class="mt-2">313 478 5277 <button id="btnregistro" class="btn btn-success">Registrate</button></h5>
+
+    
+  </div>
+</div>
+    </div>
+
+  </div>
+
+  </div> 
+		 
+		 
+
+		 
+</div>
+	  
+  </header>
+>>>>>>> test
 </body>
 </html>
